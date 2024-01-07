e3c87bb38f861c848e9c225ae3cb1aaf137867f0
AJ McKimmy <32497161+PadenFain@users.noreply.github.com>
2024-01-07 05:54:15 -0700

    added !help/!commands

--- .venv/responses.py
+++ .venv/responses.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 
-## Create seperate functions to keep command list clearner
+## Create seperate functions to keep command list cleaner
 async def chat_commands(ctx):
     if ctx.content.startswith('!balance '):
         await  ctx.channel.send(GetBalance(ctx))
@@ -18,6 +18,8 @@ async def chat_commands(ctx):
         await ctx.channel.send(Roulette(ctx))
     if ctx.content.startswith('!roulettehelp'):
         await ctx.channel.send(RouletteHelp)
+    if ctx.content.startswith('!help') or ctx.content.startswith('!commands'):
+        await ctx.channel.send(Help)
 
 ## TODO: Move all functions into a different py file.
 
@@ -64,6 +66,26 @@ def AllIn(ctx):
             df.to_csv('userinfo.csv', index=False)
         return response
 
+def AllIn(ctx):
+    df = pd.read_csv('userinfo.csv')
+    check = df['user_id'].eq(str(ctx.author)).any()
+    if check:
+        row_array = GetRowArray(ctx)
+        row_index = df.index.get_loc(df.loc[df['user_id'] == str(ctx.author)].index[0])
+        bal = int(row_array[1])
+        randchoice = random.randint(0,1)
+        if randchoice == 0:
+            bal = 500
+            df.loc[row_index] = [ctx.author, bal]
+            response = ("You lose!  New balance: 500 (Balances that fall below 500 will be raised to 500 out of pity)")
+            df.to_csv('userinfo.csv', index=False)
+        if randchoice == 1:
+            bal = bal * 2
+            df.loc[row_index] = [ctx.author, bal]
+            response = ("You win! New balance: " + str(bal))
+            df.to_csv('userinfo.csv', index=False)
+        return response
+
 ## Pretty much just GetBalance with responses that make more sense for the context
 def Register(ctx):
     df = pd.read_csv('userinfo.csv')
@@ -77,6 +99,7 @@ def Register(ctx):
         update.to_csv('userinfo.csv', mode='a', index=False, header=False)
         return ("Congratulations!  You are now registered.  Your current balance is: 500")
 
+## TODO: simplify the code in the roulette command, I should be able to pass the actual factoring into a different function and return a payout amount.
 def Roulette(ctx):
     message = ctx.content.lower()
     message_array = message.split()
@@ -133,10 +156,9 @@ def Roulette(ctx):
         response = Register(ctx)
     return response
 
-RouletteHelp = ("The roulette command needs to be formatted (!roulette [amount you wish to wager] [either a number 0-36 or red/black/green] \nThis command currently only supports single-spot or color bets."
-                "")
+RouletteHelp = ("The roulette command needs to be formatted (!roulette [amount you wish to wager] [either a number 0-36 or red/black/green] \nThis command currently only supports single-spot or color bets.")
 
-
+Help = "!register: Creates your profile.\n\n!balance: Checks your current balance.\n\n!allin: Wagers your entire balance on a 50/50 coinflip.\n\n!roulette [wager] [bet]: Bets on your selected roulette spot.\n\n!roulettehelp: More info on the roulette command."
 
 
 

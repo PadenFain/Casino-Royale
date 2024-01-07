cb5b1ae6d5751c43dca8699649945103449c8677
AJ McKimmy <32497161+PadenFain@users.noreply.github.com>
2024-01-07 05:41:10 -0700

    Added !roulette and !roulettehelp

--- .venv/responses.py
+++ .venv/responses.py
@@ -8,12 +8,16 @@ import pandas as pd
 
 ## Create seperate functions to keep command list clearner
 async def chat_commands(ctx):
-    if ctx.content.startswith('!balance'):
+    if ctx.content.startswith('!balance '):
         await  ctx.channel.send(GetBalance(ctx))
-    if ctx.content.startswith('!allin'):
+    if ctx.content.startswith('!allin '):
         await ctx.channel.send(AllIn(ctx))
-    if ctx.content.startswith('!register'):
+    if ctx.content.startswith('!register '):
         await ctx.channel.send(Register(ctx))
+    if ctx.content.startswith('!roulette '):
+        await ctx.channel.send(Roulette(ctx))
+    if ctx.content.startswith('!roulettehelp'):
+        await ctx.channel.send(RouletteHelp)
 
 ## TODO: Move all functions into a different py file.
 
@@ -47,8 +51,7 @@ def AllIn(ctx):
         row_array = GetRowArray(ctx)
         row_index = df.index.get_loc(df.loc[df['user_id'] == str(ctx.author)].index[0])
         bal = int(row_array[1])
-        randlist = [0, 1]
-        randchoice = random.choice(randlist)
+        randchoice = random.randint(0,1)
         if randchoice == 0:
             bal = 500
             df.loc[row_index] = [ctx.author, bal]
@@ -73,6 +76,66 @@ def Register(ctx):
         update = pd.DataFrame(update)
         update.to_csv('userinfo.csv', mode='a', index=False, header=False)
         return ("Congratulations!  You are now registered.  Your current balance is: 500")
+
+def Roulette(ctx):
+    message = ctx.content.lower()
+    message_array = message.split()
+    df = pd.read_csv('userinfo.csv')
+    check = df['user_id'].eq(str(ctx.author)).any()
+    if check:
+        row_array = GetRowArray(ctx)
+        row_index = df.index.get_loc(df.loc[df['user_id'] == str(ctx.author)].index[0])
+        wager = int(message_array[1])
+        bet = message_array[2]
+        bal = int(row_array[1])
+        result = random.randint(0, 36)
+
+        ## sets bools for what type of bet this is
+        try:
+            if int(row_array[1]) >= wager:
+                enough_money = True
+            elif int(row_array[1]) < wager:
+                enough_money = False
+            if message_array[2] == 'black' or message_array[2] == 'red' or message_array[2] == 'green':
+                colors = True
+            else:
+                colors = False
+
+            ## checks bools and runs bet
+            if enough_money and colors and message_array[2] != 'green':
+                if result >= 17:
+                    response = 'success'
+                    bal = bal + wager
+                else:
+                    response = 'failure'
+                    bal = bal - wager
+            if enough_money and colors and message_array[2] == 'green':
+                if result == 0:
+                    response = 'success'
+                    bal = bal + (wager * 35) - wager
+                else:
+                    response = 'failure'
+                    bal = bal - wager
+            if enough_money and not colors:
+                    if result == int(message_array[2]):
+                        response = 'success'
+                        bal = bal + (wager * 35) - wager
+                    else:
+                        response = 'failure'
+                        bal = bal - wage
+            if not enough_money:
+                response = "You don't have enough money for this wager!"
+            df.loc[row_index] = [ctx.author, bal]
+            df.to_csv('userinfo.csv', index=False)
+        except Exception:
+            response = "Exception caught.  This is likely a formatting error on your part.  Please use !roulettehelp for more info"
+    if not check:
+        response = Register(ctx)
+    return response
+
+RouletteHelp = ("The roulette command needs to be formatted (!roulette [amount you wish to wager] [either a number 0-36 or red/black/green] \nThis command currently only supports single-spot or color bets."
+                "")
+
 
 
 
